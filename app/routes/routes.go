33629a3e223ccc2e1ff728784897f18a8facc0bf
Satrio Wibowo <80750300+ropel12@users.noreply.github.com>
2023-05-16 15:33:00 +0700

    Update routes.go

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -25,12 +25,7 @@ func (r *Routes) RegisterRoutes() {
 	ro.Use(middleware.RemoveTrailingSlash())
 	ro.Use(middleware.Logger())
 	ro.Use(middleware.Recover())
-	corsConfig := middleware.CORSConfig{
-		AllowOrigins: []string{"https://education-hub-fe-3q5c.vercel.app"},
-		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
-		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
-	}
-	ro.Use(middleware.CORSWithConfig(corsConfig))
+	ro.Use(middleware.CORS())
 	//No Auth
 	ro.POST("/login", r.User.Login)
 	ro.POST("/register", r.User.Register)
