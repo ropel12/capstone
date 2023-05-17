da784196a3df6db9f5c023cfc1536f118d3ff012
satrio wibowo <wibowosatrio2002@gmail.com>
2023-05-17 13:57:51 +0700

    update  middleware

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -1,8 +1,6 @@
 package routes
 
 import (
-	"net/http"
-
 	schoolhand "github.com/education-hub/BE/app/features/school/handler"
 	userhand "github.com/education-hub/BE/app/features/user/handler"
 	"github.com/education-hub/BE/config/dependency"
@@ -24,13 +22,7 @@ func (r *Routes) RegisterRoutes() {
 	ro.Use(middleware.RemoveTrailingSlash())
 	ro.Use(middleware.Logger())
 	ro.Use(middleware.Recover())
-	corsConfig := middleware.CORSConfig{
-		AllowOrigins:     []string{"*"},
-		AllowCredentials: true,
-		AllowHeaders:     []string{"Content-Type", "withCredentials"},
-		AllowMethods:     []string{http.MethodPost, http.MethodDelete, http.MethodOptions, http.MethodPut, http.MethodGet, http.MethodPatch},
-	}
-	ro.Use(middleware.CORSWithConfig(corsConfig))
+	ro.Use(middleware.CORS())
 	//No Auth
 	ro.POST("/login", r.User.Login)
 	ro.POST("/register", r.User.Register)
